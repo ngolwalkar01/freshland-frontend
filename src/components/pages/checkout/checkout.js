facacf999d87a4c9d39097a3c75f5db9286e43b9
gurdeep111994 <gsingh22111994@gmail.com>
2024-09-10 18:01:01 +0530

    updated code

--- src/components/pages/checkout/checkout.js
+++ src/components/pages/checkout/checkout.js
@@ -447,7 +447,7 @@ function Checkout() {
 
   const validateUserAddress = () => {
     const { address_1, city, postcode, phone } = billingAddress;
-    const isNotValidBillingAddress = !(address_1 && city && postcode && phone && phone.length > 9);
+    const isNotValidBillingAddress = !(address_1 && city && postcode && phone && phone.length >= 9);
     return isNotValidBillingAddress || (userAddresses && userAddresses.length > 0 && userAddresses.some(item => item.errors && typeof item.errors === 'object' && item.errors !== null && Object.keys(item.errors).length > 0));
   }
 
